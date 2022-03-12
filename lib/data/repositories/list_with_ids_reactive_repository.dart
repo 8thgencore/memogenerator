9fed718d41e5f3b1f0318f81961c247c51cd6f9e
User <user@MacBook-Pro-User.local>
2022-03-12 17:22:04 +0300

    ListReactiveRepository

--- lib/data/repositories/list_with_ids_reactive_repository.dart
+++ lib/data/repositories/list_with_ids_reactive_repository.dart
@@ -1,56 +1,8 @@
-import 'package:rxdart/rxdart.dart';
+import 'package:memogenerator/data/repositories/list_reactive_repository.dart';
 import 'package:collection/collection.dart';
 
-abstract class ListWithIdsReactiveRepository<T> {
-  final updater = PublishSubject<Null>();
-
-  Future<List<String>> getRawData();
-
-  Future<bool> saveRawData(final List<String> items);
-
-  T convertFromString(final String rawItem);
-
-  String convertToString(final T item);
-
+abstract class ListWithIdsReactiveRepository<T> extends ListReactiveRepository<T> {
   dynamic getId(final T item);
-
-  // from Shared Preference to List
-  Future<List<T>> getItems() async {
-    final rawItems = await getRawData();
-    return rawItems.map((rawItem) => convertFromString(rawItem)).toList();
-  }
-
-  // List to jsonString
-  Future<bool> setItems(final List<T> items) {
-    final rawItems = items.map((item) => convertToString(item)).toList();
-    return _setRawItems(rawItems);
-  }
-
-  // jsonString to Shared Preference
-  Future<bool> _setRawItems(final List<String> rawItems) {
-    updater.add(null);
-    return saveRawData(rawItems);
-  }
-
-  // dynamic get List
-  Stream<List<T>> observeMemes() async* {
-    yield await getItems();
-    await for (final _ in updater) {
-      yield await getItems();
-    }
-  }
-
-  Future<bool> addItems(final T item) async {
-    final items = await getItems();
-    items.add(item);
-    return setItems(items);
-  }
-
-  Future<bool> removeItems(final T item) async {
-    final items = await getItems();
-    items.remove(item);
-    return setItems(items);
-  }
 
   // from Shared Preference to jsonString. Add new element to jsonString
   Future<bool> addItemOrReplaceById(final T newItem) async {
